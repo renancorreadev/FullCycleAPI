fd968d1b0336947ca6d14b3049d7cb8357ccec3a
renancorreadev <producer@skynance.net>
2023-08-13 18:00:50 -0300

    test: add test file to category spec

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cat extensions.txt | while read extension || [[ -n $extension ]];
-do
-  code-insiders --install-extension $extension --force
-done
--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,51 @@
 import { Category } from "./category";
+import { omit } from "lodash";
 
-describe("Category", () => {
-  it("Constructor of category", () => {
-    const category = new Category("Movie");
+describe("Category Unit Test", () => {
+  test("constructor of category", () => {
+    /** Triple AAA - Arrange act assert*/
 
-    expect(category.name).toBe("Movie");
+    let category = new Category({ name: "Movie" });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    /** @dev test pass all parameters */
+    let created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      description: "Description of movie",
+      is_active: false,
+    });
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "Description of movie",
+      is_active: false,
+      created_at,
+    });
+
+    /** @dev testing with params name and description */
+    category = new Category({
+      name: "Movie",
+      description: "other description of movie",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description of movie",
+    });
+
+    /** @dev testing pass name with is_active */
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
   });
 });
